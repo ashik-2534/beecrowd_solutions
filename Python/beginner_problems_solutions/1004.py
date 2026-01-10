e47457cd18e9d76053e0371e73ea5b746ca1a2ff
Ashik_Ridwan <ridwanashik004@gmail.com>
2026-01-10 09:13:07 +0600

    Resolved  1004 - Simple Product

--- Python/beginner_problems_solutions/1004.py
+++ Python/beginner_problems_solutions/1004.py
@@ -1,4 +1,14 @@
-A=int(input())
-B=int(input())
-prod = A*B
-print(f"PROD = {prod}")
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+A = int(input())
+B = int(input())
+
+PROD = A * B 
+
+print(f'PROD = {PROD}')
